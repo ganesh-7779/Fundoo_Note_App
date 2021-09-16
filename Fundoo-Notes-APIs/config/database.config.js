a0e2b65cc7c374ab442e2ef29b032b7012e32d46
Ganesh Gavhad <ganeshgavhad9@gmail.com>
2021-09-16 17:54:59 +0530

    feat: Added Registration API And .env file

--- Fundoo-Notes-APIs/config/database.config.js
+++ Fundoo-Notes-APIs/config/database.config.js
@@ -1,3 +1,19 @@
-module.exports = {
-    url: 'mongodb://localhost:27017/fundoo-notes'
+
+const mongoose = require('mongoose')
+const url = process.env.URL;
+
+class DBconnection {
+  database = () => {
+    mongoose.Promise = global.Promise;
+    //mongoose connect method help us to connect with DB
+    mongoose.connect(url, {
+      useNewUrlParser: true,
+    }).then(() => {
+      console.log("sucessfully connected to the database");
+    }).catch(err => {
+      console.log("Could not connect to the database. Exiting now..", err);
+      process.exit();
+    });
+  }
 }
+module.exports = new DBconnection();
--- Fundoo-Notes-APIs/server.js
+++ Fundoo-Notes-APIs/server.js
@@ -1,38 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dbConfig = require('../Fundoo-Notes-APIs/config/database.config');
-
-//object of express 
+// We import express.
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT;
+// create express app
 const app = express();
 
-// define a simple route
-app.get('/',(req, res) =>{
-    res.json({"message": "Welcome to Fundoo-Notes-Application"});
-});
-
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
-// parse application/json, basically parse incoming Request Object as a JSON Object 
+// parse requests of content-type - application/json
 app.use(express.json());
 
-// connecting to the database
-mongoose.Promise = global.Promise;
+//Conneting To the Database
+const connection =require("./config/database.config.js");
+connection.database();
 
-//mongoose connect method help us to connect with DB
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() =>{
-    console.log("sucessfully connected to the database");
-}).catch( err => {
-    console.log("Could not connect to the database. Exiting now..", err);
-    process.exit();
+// define a simple route
+app.get("/", (req, res) => {
+  res.json({ Welcome: "Welcome to the Fundoo Notes App !" });
 });
 
-// function to check server is listing or not
-function serverCheck (){
-    console.log("server is listening on port 3000");
-}
+// Require Notes routes
+require("./App/routes/user.routes.js")(app);
 
 // listen for requests
-app.listen(3000,serverCheck );
+app.listen(PORT, () => {
+  console.log(`Server is running at port no ${PORT}`);
+});
