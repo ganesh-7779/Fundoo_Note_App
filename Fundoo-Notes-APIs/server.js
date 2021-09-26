2094dbc1960d63762ef6795caf816baca19ef6e1
Ganesh Gavhad <ganeshgavhad9@gmail.com>
2021-09-26 14:35:53 +0530

    feat: Added swagger-UI for registration API with json doc format

--- Fundoo-Notes-APIs/server.js
+++ Fundoo-Notes-APIs/server.js
@@ -1,10 +1,15 @@
 // We import express.
 require("dotenv").config();
 const express = require("express");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("./swageer/swagger.json");
 const PORT = process.env.PORT;
+
+const option = {
+  explorer: true
+};
 // create express app
 const app = express();
-
 // parse requests of content-type - application/x-www-form-urlencoded
 // we can post nested object i.e // Nested Object = { person: { name: cw } }
 // extended true : allow req-body value as a json or string and array or any type
@@ -15,6 +20,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
 app.use(express.json());
+
+// swagger-ui
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc, option));
 
 // Conneting To the Database
 const connection = require("./config/database.config.js");
