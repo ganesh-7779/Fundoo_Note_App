45ebbc586cc4a8b204fa66d3f90b484cd05172a7
Ganesh Gavhad <ganeshgavhad9@gmail.com>
2021-10-07 00:09:49 +0530

    fest: Added Create note apiS

--- Fundoo-Notes-APIs/routes/user.routes.js
+++ Fundoo-Notes-APIs/routes/user.routes.js
@@ -4,10 +4,13 @@
 *****************************************/
 const userController = require("../controllers/user.controller.js");
 const middleware = require("../helper/user.helper");
+const Note = require("../controllers/notes.controller.js");
 module.exports = (app) => {
   // Create a new Note
   app.post("/register", userController.registration);
   app.post("/login", userController.loginReq);
   app.post("/forgotPassword", userController.forgotPass);
   app.put("/resetPassword", middleware.validateToken, userController.resetPass);
+
+  app.post("/createnotes", middleware.validateToken, Note.createNote);
 };
