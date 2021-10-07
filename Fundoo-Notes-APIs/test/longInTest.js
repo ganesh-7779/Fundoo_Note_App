82c4dae060aefcddf6b685eb34870a940f23291f
Ganesh Gavhad <ganeshgavhad9@gmail.com>
2021-10-07 23:56:28 +0530

    feat: Added test case with faker for create notes

--- Fundoo-Notes-APIs/test/longInTest.js
+++ Fundoo-Notes-APIs/test/longInTest.js
@@ -1,140 +1,141 @@
-/* eslint-disable eol-last */
-/* eslint-disable node/handle-callback-err */
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-undef */
-const server = require("../server");
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const data = require("./data.json");
+// /* eslint-disable eol-last */
+// /* eslint-disable node/handle-callback-err */
+// /* eslint-disable no-unused-expressions */
+// /* eslint-disable no-undef */
+// const server = require("../server");
+// const chai = require("chai");
+// const chaiHttp = require("chai-http");
+// const data = require("./data.json");
 
-chai.should();
-chai.use(chaiHttp);
-/*
- * Tase case for login APIs
- */
+// chai.should();
+// chai.use(chaiHttp);
 
-describe("Login API", () => {
-  it("whenGiven_LoginInfo_CorrectUserShould_LoggedInSuccessfully", (done) => {
-    chai
-      .request(server)
-      .post("/login")
-      .send(data.userLogin.loginInfo)
-      .end((error, res) => {
-        res.should.have.status(201);
-        res.body.should.have.property("message").eql("User logged in successfully");
-        res.body.should.have.property("success").eql(true);
-        done();
-      });
-  });
+// /*
+//  * Tase case for login APIs
+//  */
 
-  it("WhenGiven_LoginInfoPasswordWrong_ShouldReturn_UnableToLoging", (done) => {
-    chai
-      .request(server)
-      .post("/login")
-      .send(data.userLogin.wrongPassLI)
-      .end((err, res) => {
-        res.should.have.status(401);
-        res.body.should.have.property("success").eql(false);
-        res.body.should.have.property("message").eql("Unable to login. Please enter correct info");
-        done();
-      });
-  });
-  it("WhenGiven_LoginInfoEmaiWrong_ShouldReturn_UnableToLoging", (done) => {
-    chai
-      .request(server)
-      .post("/login")
-      .send(data.userLogin.wrongEmail)
-      .end((err, res) => {
-        res.should.have.status(401);
-        res.body.should.have.property("success").eql(false);
-        res.body.should.have.property("message").eql("Unable to login. Please enter correct info");
-        done();
-      });
-  });
-  it("WhenGivenPass_DoesNotMatch_withRegex_ShouldReturnStatus(422)", (done) => {
-    chai
-      .request(server)
-      .post("/login")
-      .send(data.userLogin.wrongPassER)
-      .end((err, res) => {
-        res.should.have.status(422);
-        res.body.should.have.property("success").eql(false);
-        done();
-      });
-  });
-});
+// describe("Login API", () => {
+//   it("whenGiven_LoginInfo_CorrectUserShould_LoggedInSuccessfully", (done) => {
+//     chai
+//       .request(server)
+//       .post("/login")
+//       .send(data.userLogin.loginInfo)
+//       .end((error, res) => {
+//         res.should.have.status(201);
+//         res.body.should.have.property("message").eql("User logged in successfully");
+//         res.body.should.have.property("success").eql(true);
+//         done();
+//       });
+//   });
 
-/*
- * Tase case for forget password APIs
- */
+//   it("WhenGiven_LoginInfoPasswordWrong_ShouldReturn_UnableToLoging", (done) => {
+//     chai
+//       .request(server)
+//       .post("/login")
+//       .send(data.userLogin.wrongPassLI)
+//       .end((err, res) => {
+//         res.should.have.status(401);
+//         res.body.should.have.property("success").eql(false);
+//         res.body.should.have.property("message").eql("Unable to login. Please enter correct info");
+//         done();
+//       });
+//   });
+//   it("WhenGiven_LoginInfoEmaiWrong_ShouldReturn_UnableToLoging", (done) => {
+//     chai
+//       .request(server)
+//       .post("/login")
+//       .send(data.userLogin.wrongEmail)
+//       .end((err, res) => {
+//         res.should.have.status(401);
+//         res.body.should.have.property("success").eql(false);
+//         res.body.should.have.property("message").eql("Unable to login. Please enter correct info");
+//         done();
+//       });
+//   });
+//   it("WhenGivenPass_DoesNotMatch_withRegex_ShouldReturnStatus(422)", (done) => {
+//     chai
+//       .request(server)
+//       .post("/login")
+//       .send(data.userLogin.wrongPassER)
+//       .end((err, res) => {
+//         res.should.have.status(422);
+//         res.body.should.have.property("success").eql(false);
+//         done();
+//       });
+//   });
+// });
 
-describe("Forget Password API Test Case", () => {
-  it("ForgetPassword_WithValidEmail_ShouldReturn_EmailSent", (done) => {
-    const user = data.userLogin.validEmail;
-    chai
-      .request(server)
-      .post("/forgotPassword")
-      .send(user)
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.have.property("success").eql(true);
-        res.body.should.have.property("message").eql("Email reset link sent succesfully");
-        done();
-      });
-  });
-  it("ForgetPassword_WithInValidEmail_ShouldReturn_error", (done) => {
-    const user = data.userLogin.invalidEmail;
-    chai
-      .request(server)
-      .post("/forgotPassword")
-      .send(user)
-      .end((err, res) => {
-        res.should.have.status(422);
-        done();
-      });
-  });
-});
+// /*
+//  * Tase case for forget password APIs
+//  */
 
-describe("Reset Password API", () => {
-  it("givenresetdetails_whenproper_shouldberesetlinkSent", (done) => {
-    const token = data.userLogin.userResetPasswordToken;
-    const reset = data.userLogin.validDetailss;
-    chai
-      .request(server)
-      .put("/resetPassword")
-      .set({ authorization: token })
-      .send(reset)
-      .end((error, res) => {
-        res.should.have.status(200);
-        done();
-      });
-  });
+// describe("Forget Password API Test Case", () => {
+//   it("ForgetPassword_WithValidEmail_ShouldReturn_EmailSent", (done) => {
+//     const user = data.userLogin.validEmail;
+//     chai
+//       .request(server)
+//       .post("/forgotPassword")
+//       .send(user)
+//       .end((err, res) => {
+//         res.should.have.status(200);
+//         res.body.should.have.property("success").eql(true);
+//         res.body.should.have.property("message").eql("Email reset link sent succesfully");
+//         done();
+//       });
+//   });
+//   it("ForgetPassword_WithInValidEmail_ShouldReturn_error", (done) => {
+//     const user = data.userLogin.invalidEmail;
+//     chai
+//       .request(server)
+//       .post("/forgotPassword")
+//       .send(user)
+//       .end((err, res) => {
+//         res.should.have.status(422);
+//         done();
+//       });
+//   });
+// });
 
-  it("givenresetdetails_whenproper_shouldberesetlinkSent", (done) => {
-    const token = data.userLogin.userResetPasswordToken;
-    const reset = data.userLogin.invalidDetailss;
-    chai
-      .request(server)
-      .put("/resetPassword")
-      .set({ authorization: token })
-      .send(reset)
-      .end((error, res) => {
-        res.should.have.status(422);
-        done();
-      });
-  });
+// describe("Reset Password API", () => {
+//   it("givenresetdetails_whenproper_shouldberesetlinkSent", (done) => {
+//     const token = data.userLogin.userResetPasswordToken;
+//     const reset = data.userLogin.validDetailss;
+//     chai
+//       .request(server)
+//       .put("/resetPassword")
+//       .set({ authorization: token })
+//       .send(reset)
+//       .end((error, res) => {
+//         res.should.have.status(200);
+//         done();
+//       });
+//   });
 
-  it("givenresetdetails_whenNotProper_shouldberesetlinkNotSent", (done) => {
-    const token = data.userLogin.userResetPasswordInvalidToken;
-    const reset = data.userLogin.validDetailss;
-    chai
-      .request(server)
-      .put("/resetPassword")
-      .set({ authorization: token })
-      .send(reset)
-      .end((error, res) => {
-        res.should.have.status(400);
-        done();
-      });
-  });
-});
+//   it("givenresetdetails_whenproper_shouldberesetlinkSent", (done) => {
+//     const token = data.userLogin.userResetPasswordToken;
+//     const reset = data.userLogin.invalidDetailss;
+//     chai
+//       .request(server)
+//       .put("/resetPassword")
+//       .set({ authorization: token })
+//       .send(reset)
+//       .end((error, res) => {
+//         res.should.have.status(422);
+//         done();
+//       });
+//   });
+
+//   it("givenresetdetails_whenNotProper_shouldberesetlinkNotSent", (done) => {
+//     const token = data.userLogin.userResetPasswordInvalidToken;
+//     const reset = data.userLogin.validDetailss;
+//     chai
+//       .request(server)
+//       .put("/resetPassword")
+//       .set({ authorization: token })
+//       .send(reset)
+//       .end((error, res) => {
+//         res.should.have.status(400);
+//         done();
+//       });
+//   });
+// });
--- Fundoo-Notes-APIs/test/test.js
+++ Fundoo-Notes-APIs/test/test.js
@@ -1,125 +1,125 @@
-/* eslint-disable eol-last */
-/* eslint-disable node/handle-callback-err */
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-undef */
-const server = require("../server");
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const data = require("./data.json");
-const faker = require("faker");
+// /* eslint-disable eol-last */
+// /* eslint-disable node/handle-callback-err */
+// /* eslint-disable no-unused-expressions */
+// /* eslint-disable no-undef */
+// const server = require("../server");
+// const chai = require("chai");
+// const chaiHttp = require("chai-http");
+// const data = require("./data.json");
+// const faker = require("faker");
 
-chai.should();
-chai.use(chaiHttp);
-/*
- * Test case for Registration APIs
- */
-describe("Registration API", () => {
-  it("whenGivenDetails_CorrectUserShuold_RegisterSuccessfully", (done) => {
-    const registerUser = {
-      firstName: faker.name.findName(),
-      lastName: faker.name.lastName(),
-      email: faker.internet.email(),
-      password: "GaneshG@9"
-    };
-    console.log(registerUser);
-    chai
-      .request(server)
-      .post("/register")
-      .send(registerUser)
-      .end((err, res) => {
-        res.should.have.status(201);
-        done();
-      });
-  });
+// chai.should();
+// chai.use(chaiHttp);
+// /*
+//  * Test case for Registration APIs
+//  */
+// describe("Registration API", () => {
+//   it("whenGivenDetails_CorrectUserShuold_RegisterSuccessfully", (done) => {
+//     const registerUser = {
+//       firstName: faker.name.findName(),
+//       lastName: faker.name.lastName(),
+//       email: faker.internet.email(),
+//       password: "GaneshG@9"
+//     };
+//     console.log(registerUser);
+//     chai
+//       .request(server)
+//       .post("/register")
+//       .send(registerUser)
+//       .end((err, res) => {
+//         res.should.have.status(201);
+//         done();
+//       });
+//   });
 
-  it("whenGivenDetails_HaveDuplicateUser_ShuoldReturnUserExist", (done) => {
-    const userDB = data.registration.user;
-    chai
-      .request(server)
-      .post("/register")
-      .send(userDB)
-      .end((err, res) => {
-        res.should.have.status(409);
-        res.body.should.have.property("message").eql("User already exits");
-        res.body.should.have.property("success").eql(false);
-        done();
-      });
-  });
+//   it("whenGivenDetails_HaveDuplicateUser_ShuoldReturnUserExist", (done) => {
+//     const userDB = data.registration.user;
+//     chai
+//       .request(server)
+//       .post("/register")
+//       .send(userDB)
+//       .end((err, res) => {
+//         res.should.have.status(409);
+//         res.body.should.have.property("message").eql("User already exits");
+//         res.body.should.have.property("success").eql(false);
+//         done();
+//       });
+//   });
 
-  it("whenGivenDetail_WithoutEmail_ShouldReturn_EmailRequired", (done) => {
-    chai
-      .request(server)
-      .post("/register")
-      .send(data.registration.withoutEmail)
-      .end((err, res) => {
-        res.body.should.have.property("success").eql(false);
-        done();
-      });
-  });
+//   it("whenGivenDetail_WithoutEmail_ShouldReturn_EmailRequired", (done) => {
+//     chai
+//       .request(server)
+//       .post("/register")
+//       .send(data.registration.withoutEmail)
+//       .end((err, res) => {
+//         res.body.should.have.property("success").eql(false);
+//         done();
+//       });
+//   });
 
-  it("whenGivenDetail_WithoutFirstName_ShouldReturn_FirstNameRequired", (done) => {
-    chai
-      .request(server)
-      .post("/register")
-      .send(data.registration.withoutFn)
-      .end((err, res) => {
-        res.body.should.have.property("success").eql(false);
-        done();
-      });
-  });
-  it("whenGivenDetail_WithoutLastName_ShouldReturn_LastNameRequired", (done) => {
-    chai
-      .request(server)
-      .post("/register")
-      .send(data.registration.withoutLn)
-      .end((err, res) => {
-        res.body.should.have.property("success").eql(false);
-        done();
-      });
-  });
-  it("WhenGivenEmail_DoesNotMatch_withRegex_ShouldReturnStatus(422)", (done) => {
-    chai
-      .request(server)
-      .post("/register")
-      .send(data.registration.WrongEmail)
-      .end((err, res) => {
-        res.should.have.status(422);
-        res.body.should.have.property("success").eql(false);
-        done();
-      });
-  });
+//   it("whenGivenDetail_WithoutFirstName_ShouldReturn_FirstNameRequired", (done) => {
+//     chai
+//       .request(server)
+//       .post("/register")
+//       .send(data.registration.withoutFn)
+//       .end((err, res) => {
+//         res.body.should.have.property("success").eql(false);
+//         done();
+//       });
+//   });
+//   it("whenGivenDetail_WithoutLastName_ShouldReturn_LastNameRequired", (done) => {
+//     chai
+//       .request(server)
+//       .post("/register")
+//       .send(data.registration.withoutLn)
+//       .end((err, res) => {
+//         res.body.should.have.property("success").eql(false);
+//         done();
+//       });
+//   });
+//   it("WhenGivenEmail_DoesNotMatch_withRegex_ShouldReturnStatus(422)", (done) => {
+//     chai
+//       .request(server)
+//       .post("/register")
+//       .send(data.registration.WrongEmail)
+//       .end((err, res) => {
+//         res.should.have.status(422);
+//         res.body.should.have.property("success").eql(false);
+//         done();
+//       });
+//   });
 
-  it("WhenGivenFirstName_DoesNotMatch_withRegex_ShouldReturnStatus(422)", (done) => {
-    chai
-      .request(server)
-      .post("/register")
-      .send(data.registration.WrongFn)
-      .end((err, res) => {
-        res.should.have.status(422);
-        res.body.should.have.property("success").eql(false);
-        done();
-      });
-  });
-  it("WhenGivenLastName_DoesNotMatch_withRegex_ShouldReturnStatus(422)", (done) => {
-    chai
-      .request(server)
-      .post("/register")
-      .send(data.registration.WrongLn)
-      .end((err, res) => {
-        res.should.have.status(422);
-        res.body.should.have.property("success").eql(false);
-        done();
-      });
-  });
-  it("WhenGivenPassword_DoesNotMatch_withRegex_ShouldReturnStatus(422)", (done) => {
-    chai
-      .request(server)
-      .post("/register")
-      .send(data.registration.WrongPass)
-      .end((err, res) => {
-        res.should.have.status(422);
-        res.body.should.have.property("success").eql(false);
-        done();
-      });
-  });
-});
+//   it("WhenGivenFirstName_DoesNotMatch_withRegex_ShouldReturnStatus(422)", (done) => {
+//     chai
+//       .request(server)
+//       .post("/register")
+//       .send(data.registration.WrongFn)
+//       .end((err, res) => {
+//         res.should.have.status(422);
+//         res.body.should.have.property("success").eql(false);
+//         done();
+//       });
+//   });
+//   it("WhenGivenLastName_DoesNotMatch_withRegex_ShouldReturnStatus(422)", (done) => {
+//     chai
+//       .request(server)
+//       .post("/register")
+//       .send(data.registration.WrongLn)
+//       .end((err, res) => {
+//         res.should.have.status(422);
+//         res.body.should.have.property("success").eql(false);
+//         done();
+//       });
+//   });
+//   it("WhenGivenPassword_DoesNotMatch_withRegex_ShouldReturnStatus(422)", (done) => {
+//     chai
+//       .request(server)
+//       .post("/register")
+//       .send(data.registration.WrongPass)
+//       .end((err, res) => {
+//         res.should.have.status(422);
+//         res.body.should.have.property("success").eql(false);
+//         done();
+//       });
+//   });
+// });
