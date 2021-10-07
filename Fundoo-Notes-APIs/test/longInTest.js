dd0f190ee692207d7bb0a80651310ad5609d1739
Ganesh Gavhad <ganeshgavhad9@gmail.com>
2021-10-07 23:12:54 +0530

    refactor: removed bug from create note apis

--- Fundoo-Notes-APIs/test/longInTest.js
+++ Fundoo-Notes-APIs/test/longInTest.js
@@ -13,87 +13,87 @@ chai.use(chaiHttp);
  * Tase case for login APIs
  */
 
-// describe("Login API", () => {
-//   it("whenGiven_LoginInfo_CorrectUserShould_LoggedInSuccessfully", (done) => {
-//     chai
-//       .request(server)
-//       .post("/login")
-//       .send(data.userLogin.loginInfo)
-//       .end((error, res) => {
-//         res.should.have.status(201);
-//         res.body.should.have.property("message").eql("User logged in successfully");
-//         res.body.should.have.property("success").eql(true);
-//         done();
-//       });
-//   });
+describe("Login API", () => {
+  it("whenGiven_LoginInfo_CorrectUserShould_LoggedInSuccessfully", (done) => {
+    chai
+      .request(server)
+      .post("/login")
+      .send(data.userLogin.loginInfo)
+      .end((error, res) => {
+        res.should.have.status(201);
+        res.body.should.have.property("message").eql("User logged in successfully");
+        res.body.should.have.property("success").eql(true);
+        done();
+      });
+  });
 
-//   it("WhenGiven_LoginInfoPasswordWrong_ShouldReturn_UnableToLoging", (done) => {
-//     chai
-//       .request(server)
-//       .post("/login")
-//       .send(data.userLogin.wrongPassLI)
-//       .end((err, res) => {
-//         res.should.have.status(401);
-//         res.body.should.have.property("success").eql(false);
-//         res.body.should.have.property("message").eql("Unable to login. Please enter correct info");
-//         done();
-//       });
-//   });
-//   it("WhenGiven_LoginInfoEmaiWrong_ShouldReturn_UnableToLoging", (done) => {
-//     chai
-//       .request(server)
-//       .post("/login")
-//       .send(data.userLogin.wrongEmail)
-//       .end((err, res) => {
-//         res.should.have.status(401);
-//         res.body.should.have.property("success").eql(false);
-//         res.body.should.have.property("message").eql("Unable to login. Please enter correct info");
-//         done();
-//       });
-//   });
-//   it("WhenGivenPass_DoesNotMatch_withRegex_ShouldReturnStatus(422)", (done) => {
-//     chai
-//       .request(server)
-//       .post("/login")
-//       .send(data.userLogin.wrongPassER)
-//       .end((err, res) => {
-//         res.should.have.status(422);
-//         res.body.should.have.property("success").eql(false);
-//         done();
-//       });
-//   });
-// });
+  it("WhenGiven_LoginInfoPasswordWrong_ShouldReturn_UnableToLoging", (done) => {
+    chai
+      .request(server)
+      .post("/login")
+      .send(data.userLogin.wrongPassLI)
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.should.have.property("success").eql(false);
+        res.body.should.have.property("message").eql("Unable to login. Please enter correct info");
+        done();
+      });
+  });
+  it("WhenGiven_LoginInfoEmaiWrong_ShouldReturn_UnableToLoging", (done) => {
+    chai
+      .request(server)
+      .post("/login")
+      .send(data.userLogin.wrongEmail)
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.should.have.property("success").eql(false);
+        res.body.should.have.property("message").eql("Unable to login. Please enter correct info");
+        done();
+      });
+  });
+  it("WhenGivenPass_DoesNotMatch_withRegex_ShouldReturnStatus(422)", (done) => {
+    chai
+      .request(server)
+      .post("/login")
+      .send(data.userLogin.wrongPassER)
+      .end((err, res) => {
+        res.should.have.status(422);
+        res.body.should.have.property("success").eql(false);
+        done();
+      });
+  });
+});
 
-// /*
-//  * Tase case for forget password APIs
-//  */
+/*
+ * Tase case for forget password APIs
+ */
 
-// describe("Forget Password API Test Case", () => {
-//   it("ForgetPassword_WithValidEmail_ShouldReturn_EmailSent", (done) => {
-//     const user = data.userLogin.validEmail;
-//     chai
-//       .request(server)
-//       .post("/forgotPassword")
-//       .send(user)
-//       .end((err, res) => {
-//         res.should.have.status(200);
-//         res.body.should.have.property("success").eql(true);
-//         res.body.should.have.property("message").eql("Email reset link sent succesfully");
-//         done();
-//       });
-//   });
-//   it("ForgetPassword_WithInValidEmail_ShouldReturn_error", (done) => {
-//     const user = data.userLogin.invalidEmail;
-//     chai
-//       .request(server)
-//       .post("/forgotPassword")
-//       .send(user)
-//       .end((err, res) => {
-//         res.should.have.status(422);
-//         done();
-//       });
-//   });
-// });
+describe("Forget Password API Test Case", () => {
+  it("ForgetPassword_WithValidEmail_ShouldReturn_EmailSent", (done) => {
+    const user = data.userLogin.validEmail;
+    chai
+      .request(server)
+      .post("/forgotPassword")
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property("success").eql(true);
+        res.body.should.have.property("message").eql("Email reset link sent succesfully");
+        done();
+      });
+  });
+  it("ForgetPassword_WithInValidEmail_ShouldReturn_error", (done) => {
+    const user = data.userLogin.invalidEmail;
+    chai
+      .request(server)
+      .post("/forgotPassword")
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(422);
+        done();
+      });
+  });
+});
 
 describe("Reset Password API", () => {
   it("givenresetdetails_whenproper_shouldberesetlinkSent", (done) => {
